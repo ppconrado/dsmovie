6b0bf4fbea200e28f928046cc0ce34659c9c0a3e
Jose Paulo Conrado <70010685+ppconrado@users.noreply.github.com>
2022-07-17 04:55:14 -0300

    Update index.tsx

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
           <a href="https://github.com/ppconrado">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/ppconrado</p>
             </div>
           </a>
         </div>
